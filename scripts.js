c9a118cb437e710f2f5ca7584d3c30b88ef7a5b9
Fernan <lfortega0625@gmail.com>
2023-05-10 20:34:23 -0400

    validacion de campos completa

--- scripts.js
+++ scripts.js
@@ -2,127 +2,232 @@
 
 const colors = {
     red: 'hsl(0, 100%, 67%)',
-    grey: 'hsl(0, 0%, 86%)',
-    smokeGrey: 'hsl(0, 1%, 44%)',
+    grey: 'hsl(0, 1%, 44%)',
     purple: 'hsl(259, 100%, 65%)'
 }
 
-let result = {}
-let errors = {}
+const myForm = {
+    submitError: { day: false, month: false, year: false },
+    submitResult: { day: 0 , month: 0 , year: 0 }
+}
 
 const ageForm = document.querySelector('#age-calc_form')
-
 const inputList = document.querySelectorAll('.age-calc_input')
+const hyphens = document.querySelectorAll(`.hyphens`)
 
+const verifyDay = (label, input, error_msg)=>{
 
-const changeBorderColor = (input)=>{
-    
-    const label = input.nextElementSibling
-    const error_msg = label.nextElementSibling
-    
-    let limit = 0
+    // limitar el numero de digitos (4 para los a;os)
+    if( input.value.length === 3 ) input.value = input.value.slice(0,-1) 
 
-    if(input.name === 'day' || input.name === 'month') limit = 3
-
-    if(input.name === 'year') limit = 5
-
-    // eliminar un tercer digito
-    if(input.value.length === limit) input.value = input.value.slice(0,-1) 
 
     // si el campo esta vacio poner borde gris
-    if(input.value.length === 0) {
-      
-        input.style.borderColor = colors.grey
-        error_msg.style.display = 'none'
-
-    // si el valor del campo es numero poner borde azul
-    } else if(!isNaN(input.value)) {
+    if( input.value.length === 0 ) {
         
-        // si el valor del campo no es valido poner borde morado
-        input.style.borderColor = colors.purple
-        error_msg.style.display = 'none'
-
-    } else{
-        
-        input.style.borderColor = colors.red
-
         label.style.color = colors.red
+        input.style.borderColor = colors.red
         
-        error_msg.textContent = 'Must be a valid year'
+        error_msg.textContent = 'This field is required'
         error_msg.style.display = 'block'
-        error_msg.style.color = colors.red
+        
+        myForm.submitError.month = true
+
+        return
+    } 
+    
+    // verificar si son numeros
+    if( !isNaN(input.value) && input.value > 0 && input.value < 32 ) {
+        
+        label.style.color = colors.purple
+        input.style.borderColor = colors.purple
+        
+        error_msg.style.display = 'none'
+        
+        myForm.submitError.month = false
+        
+    } else {
+        
+        label.style.color = colors.red
+        input.style.borderColor = colors.red
+        
+        error_msg.textContent = 'Must be a valid mont'
+        error_msg.style.display = 'block'
+
+        myForm.submitError.month = true
 
     } 
 
+    myForm.submitResult.day = input.value
+    
 }
 
+const verifyMonth = (label, input, error_msg)=>{
+    
+    // limitar el numero de digitos (4 para los a;os)
+    if( input.value.length === 3 ) input.value = input.value.slice(0,-1) 
 
-const verifyDay = (input)=>{
 
-    changeBorderColor(input)
+    // si el campo esta vacio poner borde gris
+    if( input.value.length === 0 ) {
+        
+        label.style.color = colors.red
+        input.style.borderColor = colors.red
+        
+        error_msg.textContent = 'This field is required'
+        error_msg.style.display = 'block'
+        
+        myForm.submitError.month = true
 
+        myForm.submitResult.month = 0
 
+        return
+    } 
+    
+    // verificar si son numeros
+    if( !isNaN(input.value) && input.value > 0 && input.value < 13 ) {
+        
+        label.style.color = colors.purple
+        input.style.borderColor = colors.purple
+        
+        error_msg.style.display = 'none'
+        
+        myForm.submitError.month = false
+        myForm.submitResult.month = input.value
+        
+    } else {
+        
+        label.style.color = colors.red
+        input.style.borderColor = colors.red
+        
+        error_msg.textContent = 'Must be a valid mont'
+        error_msg.style.display = 'block'
+
+        myForm.submitError.month = true
+        myForm.submitResult.month = 0
+
+    } 
+
+    myForm.submitResult.mont = input.value
 
 }
 
-const verifyMonth = (input)=>{
+const verifyYear = (label, input, error_msg)=>{
 
-    changeBorderColor(input)
+    let currentDate = new Date()
+
+
+    // limitar el numero de digitos (4 para los a;os)
+    if( input.value.length === 5 ) input.value = input.value.slice(0,-1) 
+
+
+    // si el campo esta vacio poner borde gris
+    if( input.value.length === 0 ) {
+        
+        label.style.color = colors.red
+        input.style.borderColor = colors.red
+        
+        error_msg.textContent = 'This field is required'
+        error_msg.style.display = 'block'
+        
+        myForm.submitError.year = true
+
+        myForm.submitResult.year = 0
+
+        return
+    } 
+    
+    // verificar si son numeros
+    if( !isNaN(input.value) ) {
+        
+        label.style.color = colors.purple
+        input.style.borderColor = colors.purple
+        
+        error_msg.style.display = 'none'
+        
+        myForm.submitError.year = false
+
+        myForm.submitResult.year = input.value
+
+        
+    } else {
+        
+        label.style.color = colors.red
+        input.style.borderColor = colors.red
+        
+        error_msg.textContent = 'Must be a valid year'
+        error_msg.style.display = 'block'
+
+        myForm.submitError.year = true
+
+        myForm.submitResult.year = 0
+
+
+    } 
+    
+    // verificar si es en el pasado
+    if( input.value > currentDate.getFullYear() ){
+
+        label.style.color = colors.red
+        input.style.borderColor = colors.red
+        
+        error_msg.textContent = 'Must be in the past'
+        error_msg.style.display = 'block'
+
+        myForm.submitError.year = true
+
+        myForm.submitResult.year = 0
+
+    }
 
 }
 
-
-const verifyYear = (input)=>{
-
+const verifyDate = (input)=>{
+    
     // label y mensaje de error de este campo
     const label = input.nextElementSibling 
     const error_msg = label.nextElementSibling
 
+    if(input.name === 'day') verifyDay(label, input, error_msg)
     
-    let currentDate = new Date()
-    
-    
-    // verificar que el año sea en el pasado
-    if(input.value > currentDate.getFullYear()){
-        
-        input.style.borderColor = colors.red
-        label.style.color = colors.red
-        
-        error_msg.textContent = 'Must be in the past'
-        error_msg.style.display = 'block'
-        error_msg.style.color = colors.red
-        
-    } else{
-        
-        label.style.color = colors.smokeGrey
-        input.style.borderColor = colors.smokeGrey
-        
-        // años calculados
-        console.log(currentDate.getFullYear() - input.value)
+    if(input.name === 'month') verifyMonth(label, input, error_msg)
 
-    }
+    if(input.name === 'year') verifyYear(label, input, error_msg)
+
+}
+
+    // no meth
+const doSomeMath = ()=>{
+
+    const {day, month, year} = myForm.submitResult
+
+    if(day === 0 || month === 0 || year === 0 ) return
+
+    const tDate = `${year}-${month}-${day}`;
+    const date = new Date(tDate);
     
+    const miliseconds = date.getTime() / 1000;
+    const seconds = miliseconds
+    const minutes = seconds / 60
+    const hours = minutes / 60
     
-    
-    // control de los bordes
-    changeBorderColor(input)
+    const days = (hours / 24) - (365*3)
+
+    const weeks = Math.floor(days / 7)
+    const months = Math.floor(weeks / 4)
+    const years = Math.floor(months / 12)
+
+    hyphens[0].textContent = years
+    hyphens[1].textContent = months
+    hyphens[2].textContent = days
+
 }
 
 
 
-
-const verifyDate = (input)=>{
-
-    if(input.name === 'day') verifyDay(input)
-    
-    if(input.name === 'month') verifyMonth(input)
-
-    if(input.name === 'year') verifyYear(input)
-
-}
 
 
 inputList.forEach( input => input.addEventListener('input', ()=> verifyDate(input) ) )
-
-
-ageForm.addEventListener( 'submit', event => event.preventDefault() )
+ageForm.addEventListener( 'submit', event => {
+    event.preventDefault() 
+    doSomeMath()
+})
